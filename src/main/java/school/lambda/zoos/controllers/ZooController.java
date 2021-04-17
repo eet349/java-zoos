6339e1fa0cf78881478396b1fba715f3a00bdf40
eet349 <51262590+eet349@users.noreply.github.com>
2021-04-16 19:00:58 -0500

    update

--- src/main/java/school/lambda/zoos/controllers/ZooController.java
+++ src/main/java/school/lambda/zoos/controllers/ZooController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.lambda.zoos.models.Zoo;
@@ -31,4 +32,10 @@ public class ZooController {
 
     //  http://localhost:2019/zoos/zoo/5
     //  all info related to a zoo
+    @GetMapping(value = "/zoo/{zooid}")
+    public ResponseEntity<?> getZooById(@PathVariable long zooid) {
+        Zoo rtnZoo = zooServices.findZooByZooid(zooid);
+
+        return new ResponseEntity<>(rtnZoo, HttpStatus.OK);
+    }
 }
